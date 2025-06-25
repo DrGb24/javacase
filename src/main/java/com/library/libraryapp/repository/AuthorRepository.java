f700eea71bc0543adb0a7f6994abeb3e9bcb14eb
Buğra Kılıçtaş <162576012+DrGb24@users.noreply.github.com>
2025-06-25 16:12:23 +0300

    Delete src directory

--- src/main/java/com/library/libraryapp/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.libraryapp.repository;
-
-import com.library.libraryapp.Entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
