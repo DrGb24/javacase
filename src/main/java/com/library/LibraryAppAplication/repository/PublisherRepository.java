3c023afe82064e4c3f5dbfd28e02382d480c9468
Buğra Kılıçtaş <162576012+DrGb24@users.noreply.github.com>
2025-06-30 11:42:28 +0300

    Delete src directory

--- src/main/java/com/library/LibraryAppAplication/repository/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.libraryapp.repository;
-
-import com.library.libraryapp.Entity.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PublisherRepository extends JpaRepository<Publisher, Long> {
-}
